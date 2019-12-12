f582d79480681eabb98c6862c8489d948b05bff7
muhamad farid padilah <bbmfrd@gmail.com>
2019-12-12 14:39:39 +0700

    sqfLite

---
+++ android/app/src/main/kotlin/crudsqlite/donati/crud_sqlite_flutter/MainActivity.kt
@@ -0,0 +1,13 @@
+package crudsqlite.donati.crud_sqlite_flutter
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
